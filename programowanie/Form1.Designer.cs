90cb4de2a5e296dce37165e5b0fc1788bbad1da2
Krzysztof Babiuch <krzysztof.babiuch97@gmail.com>
2020-07-18 14:05:04 +0200

    oprawa graficzna

--- programowanie/Form1.Designer.cs
+++ programowanie/Form1.Designer.cs
@@ -10,7 +10,14 @@
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
-      
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
@@ -246,7 +253,26 @@
 
         #endregion
 
-       
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bazaDanychToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem załadujPonownieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowyProduktToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zakończProgramToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
